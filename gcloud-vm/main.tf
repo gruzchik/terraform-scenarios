7feef057459519c3bb71d417a9a78327d1aefe24
Pro <ahtyrka@gmail.com>
2020-05-06 15:39:45 +0300

    gcloud-vm: updated count and keys

--- gcloud-vm/main.tf
+++ gcloud-vm/main.tf
@@ -6,9 +6,10 @@ provider "google" {
 
 
 resource "google_compute_instance" "default" {
-  name         = "test"
+  name         = "test-${count.index}"
   machine_type = "f1-micro"
   zone         = "us-central1-a"
+  count        = 1
 
   tags = ["foo", "bar"]
 
@@ -35,7 +36,7 @@ resource "google_compute_instance" "default" {
     foo = "bar"
   }
 
-  metadata_startup_script = "echo hi > /test.txt"
+  metadata_startup_script = "cat .ssh/id_dsa.pub >> /root/.ssh/authorized_keys2"
 
   service_account {
     scopes = ["userinfo-email", "compute-ro", "storage-ro"]
