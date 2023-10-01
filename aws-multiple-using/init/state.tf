aaee12ee5f8d13a24f97923ba7c0acfd18a5cc59
Pro <ahtyrka@gmail.com>
2023-10-02 02:53:39 +0300

    aws-multiple-using initiation

---
+++ aws-multiple-using/init/state.tf
@@ -0,0 +1,22 @@
+locals {
+  terraform_state_bucket_and_table_name = "module-structure-cloudx-terraform-state"
+}
+
+resource "aws_dynamodb_table" "remote_state_lock_table" {
+  name         = "${local.terraform_state_bucket_and_table_name}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+resource "aws_s3_bucket" "remote_state_s3_bucket" {
+  bucket = "${local.terraform_state_bucket_and_table_name}"
+
+  versioning {
+    enabled = true
+  }
+}
