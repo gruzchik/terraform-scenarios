49ff5a7d9a662dad99ce8c69b91d6fd3fe7b4909
Pro <ahtyrka@gmail.com>
2025-01-14 20:57:16 +0200

    gcloud-c-network: project initiation

---
+++ gcloud-c-network/mynetwork.tf
@@ -0,0 +1,41 @@
+# Create the mynetwork network
+resource "google_compute_network" "mynetwork" {
+name = "mynetwork"
+# RESOURCE properties go here
+auto_create_subnetworks = "true"
+}
+
+# Add a firewall rule to allow HTTP, SSH, RDP and ICMP traffic on mynetwork
+resource "google_compute_firewall" "mynetwork-allow-http-ssh-rdp-icmp" {
+name = "mynetwork-allow-http-ssh-rdp-icmp"
+# RESOURCE properties go here
+network = google_compute_network.mynetwork.self_link
+
+allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "3389"]
+    }
+allow {
+    protocol = "icmp"
+    }
+source_ranges = ["0.0.0.0/0"]
+
+}
+
+
+# Create the mynet-vm-1 instance
+module "mynet-vm-1" {
+  source           = "./instance"
+  instance_name    = "mynet-vm-1"
+  instance_zone    = "Zone"
+  instance_network = google_compute_network.mynetwork.self_link
+}
+
+# Create the mynet-vm-2" instance
+module "mynet-vm-2" {
+  source           = "./instance"
+  instance_name    = "mynet-vm-2"
+  instance_zone    = "Zone 2"
+  instance_network = google_compute_network.mynetwork.self_link
+}
+
