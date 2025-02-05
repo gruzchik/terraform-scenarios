3195e64664a8f1f98bebf0bc9b7fd32214fa43e3
Pro <ahtyrka@gmail.com>
2025-02-05 14:09:52 +0200

    qwiklabs_platform: network_name variable for 2 modular structure

--- gcloud_qwiklabs_platform/2-modular-structure/variables.tf
+++ gcloud_qwiklabs_platform/2-modular-structure/variables.tf
@@ -17,3 +17,8 @@
 variable "project_id" {
   description = "The project ID to host the network in"
 }
+
+variable "network_name" {
+  description = "The name of the VPC network being created"
+  default     = "example-vpc"
+}
