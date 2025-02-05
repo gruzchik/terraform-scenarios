ee9d012ed46edd71e7445bc4e53a5884dbe8d93a
Pro <ahtyrka@gmail.com>
2025-02-06 00:20:18 +0200

    qwiklabs_platform/2-modular-structure: module usage in second

---
+++ gcloud_qwiklabs_platform/2-modular-structure/second/outputs.tf
@@ -0,0 +1,4 @@
+output "bucket-name" {
+  description = "Bucket names."
+  value       = "module.gcs-static-website-bucket.bucket"
+}
---
+++ gcloud_qwiklabs_platform/2-modular-structure/second/variables.tf
@@ -0,0 +1,12 @@
+variable "project_id" {
+  description = "The ID of the project in which to provision resources."
+  type        = string
+  default     = "FILL IN YOUR PROJECT ID HERE"
+
+}
+
+variable "name" {
+  description = "Name of the buckets to create."
+  type        = string
+  default     = "FILL IN A (UNIQUE) BUCKET NAME HERE"
+}
