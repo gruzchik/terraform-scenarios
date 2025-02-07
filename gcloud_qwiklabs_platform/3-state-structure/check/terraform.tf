4007c2b601ff0f41e9d5566bfdf7d7e9da0ba3ee
Pro <ahtyrka@gmail.com>
2025-02-07 02:58:53 +0200

    qwiklabs_platform/3-state-structure: add check test folder

---
+++ gcloud_qwiklabs_platform/3-state-structure/check/terraform.tf
@@ -0,0 +1,22 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+terraform {
+  /* Uncomment this block to use Terraform Cloud for this tutorial
+  cloud {
+      organization = "organization-name"
+      workspaces {
+        name = "learn-terraform-import"
+      }
+  }
+  */
+
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = "~> 1.5"
+}
