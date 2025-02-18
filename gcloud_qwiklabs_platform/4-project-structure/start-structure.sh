a447e8d22afeb8fb3ea02f1db1e27386f1d00cce
Pro <ahtyrka@gmail.com>
2025-02-18 05:29:43 +0200

    qwiklabs_platform/4-project-structure: add folder structure file

---
+++ gcloud_qwiklabs_platform/4-project-structure/start-structure.sh
@@ -0,0 +1,11 @@
+touch main.tf
+touch variables.tf
+mkdir -p modules/instances
+touch modules/instances/instances.tf
+touch modules/instances/outputs.tf
+touch modules/instances/variables.tf
+
+mkdir -p modules/storage
+touch modules/storage/storage.tf
+touch modules/storage/outputs.tf
+touch modules/storage/variables.tf
